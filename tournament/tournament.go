f9b0ccb4a1ae6e96b0c603a4520f29fe5703521f
Bill Jones <misterunix@gmail.com>
2024-07-19 16:58:20 -0500

    chore: Refactor tournament system for improved readability and maintainability

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
+type Challenge struct {
+	RobotsName []string `json:"robotname"` // List of robots
+	Wins       []int    `json:"wins"`      // List of wins
+	Ties       []int    `json:"ties"`      // List of ties
+	Losses     []int    `json:"losses"`    // List of losses
+}
+
 type Robot struct {
-	Filename string // Filename of the robot
-	Count    int    // Number of times this robot has competed
+	Filename string    `json:"filename"` // Filename of the robot
+	Count    int       `json:"count"`    // Number of times this robot has competed
+	Points   int       `json:"points"`   // Points scored by this robot
+	Win      int       `json:"win"`      // Number of wins
+	Tie      int       `json:"tie"`      // Number of ties
+	Loss     int       `json:"loss"`     // Number of losses
+	Battles  Challenge `json:"battles"`  // List of battles this robot has competed agaist other robots
 }
 
 func main() {
@@ -29,73 +40,76 @@ func main() {
 		fn = strings.ToLower(fn)
 
 		if strings.HasSuffix(fn, ".bas") {
-			yt := Robot{Filename: f.Name(), Count: 0}
+			yt := Robot{}
+
 			robots = append(robots, yt)
 		}
 	}
 
-	nbots := len(robots)
-	if nbots <= 1 {
-		fmt.Println("Not enough robots to run a tournament")
-		return
-	}
-
-	// Tournament 2x2
-	for i := 0; i < nbots-1; i++ {
-		for j := i + 1; j < nbots; j++ {
-			fmt.Printf("Tournament: %s vs %s\n", robots[i].Filename, robots[j].Filename)
-			robots[i].Count++
-			robots[j].Count++
+	/*
+		nbots := len(robots)
+		if nbots <= 1 {
+			fmt.Println("Not enough robots to run a tournament")
+			return
 		}
-	}
-	for _, r := range robots {
-		fmt.Printf("%s: %d\n", r.Filename, r.Count)
-	}
-	for ii := range robots {
-		robots[ii].Count = 0
-	}
 
-	// Tournament 3x3
-	if nbots > 2 {
-		for i := 0; i < nbots-2; i++ {
-			for j := i + 1; j < nbots-1; j++ {
-				for k := j + 1; k < nbots; k++ {
-					fmt.Printf("Tournament: %s vs %s vs %s\n", robots[i].Filename, robots[j].Filename, robots[k].Filename)
-					robots[i].Count++
-					robots[j].Count++
-					robots[k].Count++
-				}
+		// Tournament 2x2
+		for i := 0; i < nbots-1; i++ {
+			for j := i + 1; j < nbots; j++ {
+				fmt.Printf("Tournament: %s vs %s\n", robots[i].Filename, robots[j].Filename)
+				robots[i].Count++
+				robots[j].Count++
 			}
 		}
 		for _, r := range robots {
 			fmt.Printf("%s: %d\n", r.Filename, r.Count)
 		}
-	}
-	for ii := range robots {
-		robots[ii].Count = 0
-	}
+		for ii := range robots {
+			robots[ii].Count = 0
+		}
 
-	// Tournament 4x4
-	if nbots > 3 {
-		for i := 0; i < nbots-3; i++ {
-			for j := i + 1; j < nbots-2; j++ {
-				for k := j + 1; k < nbots-1; k++ {
-					for l := k + 1; l < nbots; l++ {
-						fmt.Printf("Tournament: %s vs %s vs %s vs %s\n", robots[i].Filename, robots[j].Filename, robots[k].Filename, robots[l].Filename)
+		// Tournament 3x3
+		if nbots > 2 {
+			for i := 0; i < nbots-2; i++ {
+				for j := i + 1; j < nbots-1; j++ {
+					for k := j + 1; k < nbots; k++ {
+						fmt.Printf("Tournament: %s vs %s vs %s\n", robots[i].Filename, robots[j].Filename, robots[k].Filename)
 						robots[i].Count++
 						robots[j].Count++
 						robots[k].Count++
-						robots[l].Count++
 					}
 				}
 			}
+			for _, r := range robots {
+				fmt.Printf("%s: %d\n", r.Filename, r.Count)
+			}
 		}
-		for _, r := range robots {
-			fmt.Printf("%s: %d\n", r.Filename, r.Count)
+		for ii := range robots {
+			robots[ii].Count = 0
 		}
-	}
-	for ii := range robots {
-		robots[ii].Count = 0
-	}
+
+		// Tournament 4x4
+		if nbots > 3 {
+			for i := 0; i < nbots-3; i++ {
+				for j := i + 1; j < nbots-2; j++ {
+					for k := j + 1; k < nbots-1; k++ {
+						for l := k + 1; l < nbots; l++ {
+							fmt.Printf("Tournament: %s vs %s vs %s vs %s\n", robots[i].Filename, robots[j].Filename, robots[k].Filename, robots[l].Filename)
+							robots[i].Count++
+							robots[j].Count++
+							robots[k].Count++
+							robots[l].Count++
+						}
+					}
+				}
+			}
+			for _, r := range robots {
+				fmt.Printf("%s: %d\n", r.Filename, r.Count)
+			}
+		}
+		for ii := range robots {
+			robots[ii].Count = 0
+		}
+	*/
 
 }
